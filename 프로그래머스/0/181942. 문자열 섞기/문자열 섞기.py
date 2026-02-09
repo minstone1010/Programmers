d579395e20836d0ca042441f25e788e076839087
minstone1010 <101106601+minstone1010@users.noreply.github.com>
2026-02-09 19:49:46 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -0,0 +1,11 @@
+def solution(str1, str2):
+    str1 = list(str1)
+    str2 = list(str2)
+    answer = []
+    
+    for i in range(len(str1)):
+        answer.append(str1[i])
+        answer.append(str2[i])
+        
+    answer = ''.join(answer)
+    return answer
