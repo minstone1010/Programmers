762b805ecda9bf3a63143bc0c8b2b63ef6ba1dad
minstone1010 <101106601+minstone1010@users.noreply.github.com>
2026-02-16 21:06:44 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,10 @@
+def solution(num):
+    answer = -1
+    for i in range(500):
+        if num == 1:
+            return i
+        elif num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+    return answer
