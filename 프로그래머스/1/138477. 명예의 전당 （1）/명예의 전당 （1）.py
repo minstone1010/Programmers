d48ae33b9cd455bc8bf703e09c6b46ca48bbd44d
minstone1010 <101106601+minstone1010@users.noreply.github.com>
2026-03-04 23:05:32 +0900

    [level 1] Title: 명예의 전당 (1), Time: 9.09 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -3,8 +3,7 @@ def solution(k, score):
     klist = []
     for i in score:
         klist.append(i)
-        klist.sort(reverse=True)
         if len(klist) > k:
-            klist.pop()
-        answer.append(klist[len(klist)-1])
+            klist.remove(min(klist))
+        answer.append(min(klist))
     return answer
