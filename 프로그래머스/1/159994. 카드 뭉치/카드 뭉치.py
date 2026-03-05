6543484cddd9640c0f66eb6695d66f72a6075681
minstone1010 <101106601+minstone1010@users.noreply.github.com>
2026-03-05 22:46:38 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    i = 0
+    j = 0
+    
+    for word in goal:
+        if i < len(cards1) and word == cards1[i]:
+            i += 1
+        elif j < len(cards2) and word == cards2[j]:
+            j += 1
+        else:
+            return "No"
+    
+    return "Yes"
